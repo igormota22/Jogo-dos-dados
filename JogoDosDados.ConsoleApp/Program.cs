4fcfc08d9768f3039713ee7d6674135eb22012f9
Igor Mota <igormello399@gmail.com>
2026-03-19 14:36:52 -0300

    Adicionada casas de avanço e recuo

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -3,7 +3,7 @@
 class Program
 {
     /*
-    Jogador e computador começam na posição 0
+ Jogador e computador começam na posição 0
  A cada rodada, um dado é sorteado (1 a 6)
  O valor é somado à posição atual
  Existem casas com eventos especiais
@@ -13,6 +13,8 @@ class Program
     static void Main(string[] args)
     {
       const int limiteLinhaDeChegada = 30;
+      const int bonusAvancoExtra = 3;
+      const int penalidadeRecuo = 2;
 
         while (true)
         {
@@ -35,15 +37,27 @@ class Program
 
             posicaoJogador += resultado;
 
-            if(posicaoJogador < limiteLinhaDeChegada)
-            {
-                System.Console.WriteLine($"Voce esta na posição {posicaoJogador} de {limiteLinhaDeChegada}");
-            }
-                else
+            System.Console.WriteLine($"Voce esta na posição {posicaoJogador} de {limiteLinhaDeChegada}");
+
+            if (posicaoJogador == 5 || posicaoJogador == 10 || posicaoJogador == 15 || posicaoJogador == 25)
                 {
-                    System.Console.WriteLine("Parabéns, voce chegou na linha de chegada!!");
-                    jogoEstaEmAndamento = false;
+
+                    System.Console.WriteLine("\nCasa de Evento.Avance mais 3 posiçoes");
+                    posicaoJogador += bonusAvancoExtra;
                 }
+                else if (posicaoJogador == 7 || posicaoJogador == 13 || posicaoJogador == 20)
+                {
+                    System.Console.WriteLine("\nCasa de Evento.Recue menos 3 posiçoes");
+                    posicaoJogador -= penalidadeRecuo;
+                }
+
+            if(posicaoJogador >= limiteLinhaDeChegada)
+            {
+                System.Console.WriteLine("Parabéns, voce chegou na linha de chegada!!");
+                jogoEstaEmAndamento = false;;
+            }
+
+            System.Console.WriteLine();    
             System.Console.WriteLine("Pressione ENTER para continuar");
             Console.ReadLine();
             }
