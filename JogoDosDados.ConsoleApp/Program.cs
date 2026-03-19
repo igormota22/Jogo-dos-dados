c46b4bfba7bd9afa6a0ca6a91b647b76c6c2c86d
Igor Mota <igormello399@gmail.com>
2026-03-19 14:17:10 -0300

    Adicionado loop de andamento e condição de vitoria

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -12,8 +12,16 @@ class Program
     */
     static void Main(string[] args)
     {
+      const int limiteLinhaDeChegada = 30;
+
         while (true)
         {
+            int posicaoJogador = 0;
+            bool jogoEstaEmAndamento = true;
+
+            while (jogoEstaEmAndamento){
+
+            Console.Clear();
             System.Console.WriteLine("----------------------");
             System.Console.WriteLine("JOGO DOS DADOS");
             System.Console.WriteLine("----------------------");
@@ -24,6 +32,21 @@ class Program
             int resultado = RandomNumberGenerator.GetInt32(1,7);
 
             System.Console.WriteLine($"O número sorteado foi {resultado}");
+
+            posicaoJogador += resultado;
+
+            if(posicaoJogador < limiteLinhaDeChegada)
+            {
+                System.Console.WriteLine($"Voce esta na posição {posicaoJogador} de {limiteLinhaDeChegada}");
+            }
+                else
+                {
+                    System.Console.WriteLine("Parabéns, voce chegou na linha de chegada!!");
+                    jogoEstaEmAndamento = false;
+                }
+            System.Console.WriteLine("Pressione ENTER para continuar");
+            Console.ReadLine();
+            }
 
             System.Console.Write("Deseja continuar? (s/N): ");
             string? opcaoDesejaContinuar = Console.ReadLine()?.ToUpper();
